5b587cf7b12c9393036f375dbe69f0e14d2feee9
Oleh Kozlovskyi <oleh.kozlovskyi@eleks.com>
2018-02-01 17:05:29 +0200

    add routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './css/style.css';
+
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Routing = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={StorePicker}/>
+        <Route path="store/:storeId" component={App}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </BrowserRouter>
+  )
+};
+
+ReactDOM.render(<Routing />, document.getElementById('root'));
 
